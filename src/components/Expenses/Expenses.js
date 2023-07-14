057239fbbe529e3bb27da03dfb16a230f39bb043
UBC Student <2860128966@qq.com>
2023-07-13 23:15:52 -0700

    lift the state up

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,15 @@
 import ExpenseItem from './ExpenseItem';
 import "./Expenses.css";
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+import { useState } from 'react';
+
 const Expenses = ({items}) => {
+  const [selected, setSelected] = useState("2020");
+
   return (
     <Card className='expenses'>
+    <ExpensesFilter select={selected} onSelectChange={setSelected}/>
        {items.map((item) =>(
          <ExpenseItem
          title={item.title}
@@ -12,7 +18,7 @@ const Expenses = ({items}) => {
        />
        ))}
     </Card>
-  );
+ );
 }
 
 export default Expenses
